f6926c9b2154148efc7dc68f71c8d23a0308506f
Mohamed Fathy <codetwostart@gmail.com>
2019-11-01 17:45:55 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace classes_and_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object syntax
+            //type identifier = new type();
+            Student s1 = new Student();
+            s1.name = "Ahmed Mohamed";
+            Console.WriteLine(s1.name);
+            Teacher t1 = new Teacher();
+            t1.name = "Mohamed Fathy";
+            Console.WriteLine(t1.name);
+        }
+    }
+    class Student
+    {
+        //class members
+        //fiels = class data
+        public string name;
+
+    }
+    struct Teacher
+    {
+        //fiels = class data
+        public string name;
+    }
+
+}
